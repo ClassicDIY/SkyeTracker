6e9b3b2b2e456a34b259b41a1a006f86ef8d20dc
Thijs Triemstra <info@collab.nl>
2020-03-08 18:46:28 +0100

    move anemometer config to build_flags

--- code/ESP32/SkyeTracker/src/Anemometer.cpp
+++ code/ESP32/SkyeTracker/src/Anemometer.cpp
@@ -21,12 +21,12 @@ namespace SkyeTracker
 			return 0;
 		}
 		double sensorVoltage = -0.000000000000016 * pow(reading, 4) + 0.000000000118171 * pow(reading, 3) - 0.000000301211691 * pow(reading, 2) + 0.001109019271794 * reading + 0.034143524634089;
-		//Convert voltage value to wind speed using range of max and min voltages and wind speed for the anemometer
-		if (sensorVoltage <= voltageMin) {
-			return 0; //Check if voltage is below minimum value. If so, set wind speed to zero.
+		// Convert voltage value to wind speed using range of max and min voltages and wind speed for the anemometer
+		if (sensorVoltage <= AnemometerVoltageMin) {
+			return 0; // Check if voltage is below minimum value. If so, set wind speed to zero.
 		}
 		else {
-			return (sensorVoltage - voltageMin)*windSpeedMax / (voltageMax - voltageMin); //For voltages above minimum value, use the linear relationship to calculate wind speed.
+			return (sensorVoltage - AnemometerVoltageMin)*AnemometerWindSpeedMax / (AnemometerVoltageMax - AnemometerVoltageMin); // For voltages above minimum value, use the linear relationship to calculate wind speed.
 		}
 	}
 }
