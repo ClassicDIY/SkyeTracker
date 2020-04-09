bb49478ad51ff8a24da74c09aecd67530e894d3b
Graham <graham.a.ross@gmail.com>
2020-04-09 13:55:00 -0400

    Added support for ESPFlashTool

--- code/ESP32/SkyeTracker/include/IOT.h
+++ code/ESP32/SkyeTracker/include/IOT.h
@@ -11,7 +11,7 @@ extern "C"
 #include "IotWebConf.h"
 #include "Configuration.h"
 
-#define CONFIG_VERSION "V1.0"
+#define CONFIG_VERSION "V1.0.1" // major.minor.build (major or minor will invalidate the configuration)
 #define HOME_ASSISTANT_PREFIX "homeassistant" // MQTT prefix used in autodiscovery
 #define STR_LEN 64                            // general string buffer size
 #define CONFIG_LEN 32                         // configuration string buffer size
@@ -29,6 +29,6 @@ namespace SkyeTracker
         void publish(const char *subtopic, const char *value, boolean retained = false);
 
     private:
-
+        bool _clientsConfigured = false;
     };
 } // namespace SkyeTracker
--- code/ESP32/SkyeTracker/src/IOT.cpp
+++ code/ESP32/SkyeTracker/src/IOT.cpp
@@ -1,5 +1,6 @@
 #include "IOT.h"
 #include <sys/time.h>
+#include <EEPROM.h>
 #include "time.h"
 #include "Log.h"
 #include "Tracker.h"
@@ -23,10 +24,10 @@ char _mqttUserPassword[IOTWEBCONF_WORD_LEN];
 u_int _uniqueId = 0;
 IotWebConfSeparator seperatorParam = IotWebConfSeparator("MQTT");
 IotWebConfParameter mqttServerParam = IotWebConfParameter("MQTT server", "mqttServer", _mqttServer, IOTWEBCONF_WORD_LEN);
-IotWebConfParameter mqttPortParam = IotWebConfParameter("MQTT port", "mqttSPort", _mqttPort, 5, "text", NULL, "8080");
+IotWebConfParameter mqttPortParam = IotWebConfParameter("MQTT port", "mqttSPort", _mqttPort, 5, "text", NULL, "1883");
 IotWebConfParameter mqttUserNameParam = IotWebConfParameter("MQTT user", "mqttUser", _mqttUserName, IOTWEBCONF_WORD_LEN);
 IotWebConfParameter mqttUserPasswordParam = IotWebConfParameter("MQTT password", "mqttPass", _mqttUserPassword, IOTWEBCONF_WORD_LEN, "password");
-
+IotWebConfParameter mqttRootTopicParam = IotWebConfParameter("MQTT Root Topic", "mqttRootTopic", _mqttRootTopic, IOTWEBCONF_WORD_LEN);
 const char *ntpServer = "pool.ntp.org";
 
 void publishDiscovery()
@@ -62,25 +63,25 @@ void publishDiscovery()
 
 void onMqttConnect(bool sessionPresent)
 {
-	logi("Connected to MQTT. Session present: %d", sessionPresent);
+	logd("Connected to MQTT. Session present: %d", sessionPresent);
 	char mqttCmndTopic[STR_LEN];
 	sprintf(mqttCmndTopic, "%s/cmnd/Mode", _mqttRootTopic);
 	uint16_t packetIdSub = _mqttClient.subscribe(mqttCmndTopic, 1);
 	sprintf(mqttCmndTopic, "%s/cmnd/MoveTo", _mqttRootTopic);
 	packetIdSub = _mqttClient.subscribe(mqttCmndTopic, 1);
-	logi("MQTT subscribe, QoS 1, packetId: %d", packetIdSub);
+	logd("MQTT subscribe, QoS 1, packetId: %d", packetIdSub);
 	publishDiscovery();
 	_mqttClient.publish(_willTopic, 0, false, "Online");
 }
 
 void onMqttDisconnect(AsyncMqttClientDisconnectReason reason)
 {
-	logi("Disconnected from MQTT. Reason: %d", (int8_t)reason);
+	logw("Disconnected from MQTT. Reason: %d", (int8_t)reason);
 }
 
 void connectToMqtt()
 {
-	logi("Connecting to MQTT...");
+	logd("Connecting to MQTT...");
 	if (WiFi.isConnected())
 	{
 		_mqttClient.connect();
@@ -89,17 +90,24 @@ void connectToMqtt()
 
 void WiFiEvent(WiFiEvent_t event)
 {
-	logi("[WiFi-event] event: %d", event);
+	logd("[WiFi-event] event: %d", event);
+	String s;
+	StaticJsonDocument<128> doc;
 	switch (event)
 	{
 	case SYSTEM_EVENT_STA_GOT_IP:
-		logi("WiFi connected, IP address: %s", WiFi.localIP().toString().c_str());
+		// logd("WiFi connected, IP address: %s", WiFi.localIP().toString().c_str());
+		doc["IP"] = WiFi.localIP().toString().c_str();
+		doc["ApPassword"] = TAG;
+		serializeJson(doc, s);
+		s += '\n';
+		Serial.printf(s.c_str()); // send json to flash tool
 		configTime(0, 0, ntpServer);
 		printLocalTime();
 		xTimerStart(mqttReconnectTimer, 0);
 		break;
 	case SYSTEM_EVENT_STA_DISCONNECTED:
-		logi("WiFi lost connection");
+		logw("WiFi lost connection");
 		xTimerStop(mqttReconnectTimer, 0); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
 		break;
 	default:
@@ -109,18 +117,18 @@ void WiFiEvent(WiFiEvent_t event)
 
 void onMqttPublish(uint16_t packetId)
 {
-	logi("Publish acknowledged.  packetId: %d", packetId);
+	logd("Publish acknowledged.  packetId: %d", packetId);
 }
 
 void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total)
 {
-	logi("MQTT Message arrived [%s]  qos: %d len: %d index: %d total: %d", topic, properties.qos, len, index, total);
+	logd("MQTT Message arrived [%s]  qos: %d len: %d index: %d total: %d", topic, properties.qos, len, index, total);
 	printHexString(payload, len);
 	int l = strlen(_mqttRootTopic) + 6;
 	if (l < strlen(topic) && len < 8)
 	{
 		char *p = &topic[l];
-		logi("p: %s", p);
+		logd("p: %s", p);
 		char cmd[16];
 		char pl[16];
 		if (strcmp("Mode", p) == 0)
@@ -128,7 +136,7 @@ void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties 
 			strncpy(pl, payload, len);
 			pl[len] = 0;
 			sprintf(cmd, "%s|", pl);
-			logi("cmd: %s", cmd);
+			logd("cmd: %s", cmd);
 			_tracker.ProcessCommand(cmd);
 		}
 		if (strcmp("MoveTo", p) == 0)
@@ -137,7 +145,7 @@ void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties 
 			strncpy(pl, payload, len);
 			pl[len] = 0;
 			sprintf(cmd, "MoveTo|%s", pl);
-			logi("cmd: %s", cmd);
+			logd("cmd: %s", cmd);
 			_tracker.ProcessCommand(cmd);
 		}
 	}
@@ -155,11 +163,11 @@ void handleRoot()
 	// -- Let IotWebConf test and handle captive portal requests.
 	if (_iotWebConf.handleCaptivePortal())
 	{
-		logi("Captive portal");
+		logd("Captive portal");
 		// -- Captive portal request were already served.
 		return;
 	}
-	logi("handleRoot");
+	logd("handleRoot");
 	String s = "<!DOCTYPE html><html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/>";
 	s += "<title>SkyeTracker</title></head><body>";
 	s += _iotWebConf.getThingName();
@@ -186,6 +194,20 @@ void handleRoot()
 
 void IOT::Init()
 {
+	pinMode(FACTORY_RESET_PIN, INPUT_PULLUP);
+	_iotWebConf.setStatusPin(WIFI_STATUS_PIN);
+	_iotWebConf.setConfigPin(WIFI_AP_PIN);
+	if (digitalRead(FACTORY_RESET_PIN) == LOW)
+	{
+		EEPROM.begin(IOTWEBCONF_CONFIG_START + IOTWEBCONF_CONFIG_VERSION_LENGTH );
+		for (byte t = 0; t < IOTWEBCONF_CONFIG_VERSION_LENGTH; t++)
+		{
+			EEPROM.write(IOTWEBCONF_CONFIG_START + t, 0);
+		}
+		EEPROM.commit();
+		EEPROM.end();
+		logw("Factory Reset!");
+	}
 	mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(5000), pdFALSE, (void *)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
 	WiFi.onEvent(WiFiEvent);
 	_iotWebConf.setupUpdateServer(&_httpUpdater);
@@ -194,48 +216,108 @@ void IOT::Init()
 	_iotWebConf.addParameter(&mqttPortParam);
 	_iotWebConf.addParameter(&mqttUserNameParam);
 	_iotWebConf.addParameter(&mqttUserPasswordParam);
+	_iotWebConf.addParameter(&mqttRootTopicParam);
 	boolean validConfig = _iotWebConf.init();
 	if (!validConfig)
 	{
-		loge("!invalid configuration!");
+		logw("!invalid configuration!");
 		_mqttServer[0] = '\0';
 		_mqttPort[0] = '\0';
 		_mqttUserName[0] = '\0';
 		_mqttUserPassword[0] = '\0';
+		strcpy(_mqttRootTopic, _iotWebConf.getThingName());
+		_iotWebConf.resetWifiAuthInfo();
 	}
 	else
 	{
-		_iotWebConf.setApTimeoutMs(AP_TIMEOUT);
+		_iotWebConf.skipApStartup(); // Set WIFI_AP_PIN to gnd to force AP mode
+		if (_mqttServer[0] != '\0') // skip if factory reset
+		{
+			logd("Valid configuration!");
+			_clientsConfigured = true;
+			// setup MQTT
+			_mqttClient.onConnect(onMqttConnect);
+			_mqttClient.onDisconnect(onMqttDisconnect);
+			_mqttClient.onMessage(onMqttMessage);
+			_mqttClient.onPublish(onMqttPublish);
+			IPAddress ip;
+			if (ip.fromString(_mqttServer))
+			{
+				int port = atoi(_mqttPort);
+				_mqttClient.setServer(ip, port);
+				_mqttClient.setCredentials(_mqttUserName, _mqttUserPassword);
+				sprintf(_willTopic, "%s/tele/LWT", _mqttRootTopic);
+				_mqttClient.setWill(_willTopic, 0, false, "Offline");
+			}
+		}
 	}
 	// Set up required URL handlers on the web server.
 	_webServer.on("/", handleRoot);
 	_webServer.on("/config", [] { _iotWebConf.handleConfig(); });
 	_webServer.onNotFound([]() { _iotWebConf.handleNotFound(); });
-	_mqttClient.onConnect(onMqttConnect);
-	_mqttClient.onDisconnect(onMqttDisconnect);
-	_mqttClient.onMessage(onMqttMessage);
-	_mqttClient.onPublish(onMqttPublish);
-	// generate unique id from mac address NIC segment
-	uint8_t chipid[6];
-	esp_efuse_mac_get_default(chipid);
-	_uniqueId = chipid[3] << 16;
-	_uniqueId += chipid[4] << 8;
-	_uniqueId += chipid[5];
-	sprintf(_mqttRootTopic, "%s/%X/solar", _iotWebConf.getThingName(), _uniqueId);
-	IPAddress ip;
-	if (ip.fromString(_mqttServer))
-	{
-		int port = atoi(_mqttPort);
-		_mqttClient.setServer(ip, port);
-		_mqttClient.setCredentials(_mqttUserName, _mqttUserPassword);
-		sprintf(_willTopic, "%s/tele/LWT", _mqttRootTopic);
-		_mqttClient.setWill(_willTopic, 0, false, "Offline");
-	}
 }
 
 void IOT::Run()
 {
 	_iotWebConf.doLoop();
+	if (_clientsConfigured && WiFi.isConnected())
+	{
+		// ToDo MQTT monitoring
+		// if (_mqttClient.connected())
+		// {
+		// 	if (_lastPublishTimeStamp < millis())
+		// 	{
+		// 		_lastPublishTimeStamp = millis() + _currentPublishRate;
+		// 		_publishCount++;
+		// 		publishReadings();
+		// 		// Serial.printf("%d ", _publishCount);
+		// 	}
+		// 	if (!_stayAwake && _publishCount >= WAKE_COUNT)
+		// 	{
+		// 		_publishCount = 0;
+		// 		_currentPublishRate = SNOOZE_PUBLISH_RATE;
+		// 		logd("Snoozing!");
+		// 	}
+		// }
+	}
+	else
+	{
+		if (Serial.peek() == '{')
+		{
+			String s = Serial.readStringUntil('}');
+			s += "}";
+			StaticJsonDocument<128> doc;
+			DeserializationError err = deserializeJson(doc, s);
+			if (err)
+			{
+				loge("deserializeJson() failed: %s", err.c_str());
+			}
+			else
+			{
+				if (doc.containsKey("ssid") && doc.containsKey("password"))
+				{
+					IotWebConfParameter *p = _iotWebConf.getWifiSsidParameter();
+					strcpy(p->valueBuffer, doc["ssid"]);
+					logd("Setting ssid: %s", p->valueBuffer);
+					p = _iotWebConf.getWifiPasswordParameter();
+					strcpy(p->valueBuffer, doc["password"]);
+					logd("Setting password: %s", p->valueBuffer);
+					p = _iotWebConf.getApPasswordParameter();
+					strcpy(p->valueBuffer, TAG); // reset to default AP password
+					_iotWebConf.configSave();
+					esp_restart(); // force reboot
+				}
+				else
+				{
+					logw("Received invalid json: %s", s.c_str());
+				}
+			}
+		}
+		else
+		{
+			Serial.read(); // discard data
+		}
+	}
 }
 
 void IOT::publish(const char *subtopic, const char *value, boolean retained)
