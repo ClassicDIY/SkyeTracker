0c4d336cd54e12319e690971f02a92fc6b7c60e3
Graham <graham.a.ross@gmail.com>
2020-04-18 09:51:14 -0400

    Moved version and root topic to platform.ini

--- code/ESP32/include/IOT.h
+++ code/ESP32/include/IOT.h
@@ -11,8 +11,6 @@ extern "C"
 #include "IotWebConf.h"
 #include "Configuration.h"
 
-#define CONFIG_VERSION "V1.0.2" // major.minor.build (major or minor will invalidate the configuration)
-#define HOME_ASSISTANT_PREFIX "homeassistant" // MQTT prefix used in autodiscovery
 #define STR_LEN 64                            // general string buffer size
 #define CONFIG_LEN 32                         // configuration string buffer size
 
